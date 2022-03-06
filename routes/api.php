JonathanGawrych/fa-search

Convert everything to tabs

routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/whats-my-ip', static function (Request $request): ?string {
-    return $request->ip();
+	return $request->ip();
 });

JonathanGawrych/app-template

routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });